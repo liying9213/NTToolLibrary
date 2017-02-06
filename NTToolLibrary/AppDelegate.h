7edcfa57c5283347ad30b2d9dc4631626894e2d5
liying <liying@medbigbang.com>
2017-02-06 13:45:02 +0800

    Initial Commit

---
+++ NTToolLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NTToolLibrary
+//
+//  Created by NTTian on 2017/2/6.
+//  Copyright © 2017年 liying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NTToolLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NTToolLibrary
+//
+//  Created by NTTian on 2017/2/6.
+//  Copyright © 2017年 liying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
